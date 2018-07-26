97ac6cef25a0a17701687ec953856f036e49b615
Carolyn Lea <carolynrlea@gmail.com>
2018-07-26 14:41:26 -0600

    Add code to VotingViewController, grouped files

---
+++ Poll/Poll/ViewControllers/VotingViewController.swift
@@ -0,0 +1,28 @@
+//
+//  VotingViewController.swift
+//  Poll
+//
+//  Created by Carolyn Lea on 7/26/18.
+//  Copyright © 2018 Carolyn Lea. All rights reserved.
+//
+
+import UIKit
+
+class VotingViewController: UIViewController, VoteControllerProtocol {
+
+    @IBOutlet weak var nameTextfield: UITextField!
+    
+    @IBOutlet weak var colorTextfield: UITextField!
+    
+    var voteController: VoteController?
+    
+    @IBAction func save(_ sender: Any) {
+        
+        guard let name = nameTextfield.text,
+            let response = colorTextfield.text else {return}
+        
+        voteController?.createVote(with: name, response: response)
+        
+    }
+    
+}
